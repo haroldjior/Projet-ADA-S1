ad1cbe1b0ad47bde46496b8a2158c7a549cd2a6d
Ilan <guerizec.ilan03@gmail.com>
2025-11-07 11:32:05 +0100

    ajout du package Gestion_date, sans les gestion d'erreurs type Constraint_error

--- src/test.adb
+++ src/test.adb
@@ -1,7 +1,11 @@
-with ada.text_io, ada.integer_text_io; use ada.text_io, ada.integer_text_io;
+with ada.text_io, ada.integer_text_io, gestion_date;
+use ada.text_io, ada.integer_text_io, gestion_date;
 
 procedure test is
-
+   date       : T_date;
+   tab_mois   : T_tab_mois;
+   liste_mois : T_liste_mois;
 begin
-   put ("Hello World");
+   ini_tab_mois (tab_mois, liste_mois);
+   saisie_date (date, tab_mois);
 end test;
